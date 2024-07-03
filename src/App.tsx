2b7fdc12af963b7d5eb18da7c08456b08b170cf3
Sean Collins <sean.collins@outlook.com>
2024-07-03 16:01:41 -0700

    Load Forge components and build default scaffold

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-import './App.css'
+import { defineComponents } from '@tylertech/forge'
+import '@tylertech/forge/dist/forge.css';
+
+defineComponents();
 
 function App() {
   return (
     <>
-
+      <forge-scaffold>
+        <forge-app-bar slot="header" title-text="My Application"></forge-app-bar>
+        <forge-toolbar slot="body-header">
+          <h2 className="forge-typography--heading4">My Page</h2>
+        </forge-toolbar>
+        <main slot="body">
+          <forge-card>
+            <p>My Content</p>
+          </forge-card>
+        </main>
+        <forge-footer slot="footer">
+          <p>My Footer</p>
+        </forge-footer>
+      </forge-scaffold>
     </>
   )
 }
